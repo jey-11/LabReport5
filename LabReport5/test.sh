f54596efd1c00f79bf8787d7a8fcca708b0f11e3
jey-11 <137555229+jey-11@users.noreply.github.com>
2023-12-01 10:20:10 -0800

    Delete LabReport5 directory

--- LabReport5/test.sh
+++
@@ -1,6 +0,0 @@
-rm -rf compiled
-mkdir compiled 
-javac -d target -cp target:junit-platform-console-standalone-1.7.2.jar /Users/jeyas/LabReport5/Test.java 
-java -jar junit-platform-console-standalone-1.7.2.jar --class-path compiled Test > output.txt
-cat output.txt
-
